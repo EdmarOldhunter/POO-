cb7c2f75c4c21c6d6104e5d069c86d76bb321296
Perales <e.perales@dxc.com>
2022-06-17 16:25:38 +0000

    fixed class carro and create main package

---
+++ src/main/Carro.java
@@ -0,0 +1,60 @@
+package main;
+
+public class Carro {
+	
+	//Atributos
+	String color; 
+	String carroseria;
+	String llantas;
+	String ventanas;
+	String Puertas; 
+	
+	
+	//Constructores
+	public Carro() {
+	
+		
+				
+	}
+	
+	
+	public Carro(String carroseria, String llantas, String ventanas, String Puertas, String color) {
+		
+		this.color = color;
+		this.carroseria = carroseria;
+		this.llantas =  llantas; 
+		this.ventanas = ventanas;
+		this.Puertas = Puertas;
+        
+	}
+	
+	
+	
+	
+	
+	
+	
+	//Metodos
+	public void Avanzar() {
+		
+	
+	}
+	
+    public void retroceder(){
+    	
+    
+    
+    }
+    
+    public void vueltaderecha(){
+    	
+    	
+    	
+    }
+    
+    public void vueltaizquierda(){
+    	
+    
+    }
+}
+    
--- src/main/main_carro.java
+++ src/main/main_carro.java
@@ -1,3 +1,4 @@
+package main;
 
 public class main_carro {
 
